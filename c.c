2ee1a4ba25d903ce1667ff0172098aa453389ba4
Harshght <harshkumargog@gmail.com>
2025-04-18 04:26:42 +0530

    code to code

---
+++ c.c
@@ -0,0 +1,48 @@
+// // #include<stdio.h>
+// // int main(){
+// //     int a, b;
+// //     a = 2, b = 3;
+    
+// //     printf("sum is %d",a + b);
+// // return 0;
+// // }
+
+// //  #include<stdio.h>
+// //  int main(){
+// //     float pi = 3.14;
+// //     float radius = 5.0;
+// //     float area = pi * radius * radius;
+// //     printf("Area of circle is %f", area);
+    
+// //  return 0;
+// //  }
+
+
+// #include<stdio.h>
+// int main(){
+//     // celsius to fahrenheit
+//     float celsius = 25.0;
+//     float fahrenheit = (celsius * 9/5) + 32;
+//     printf("Temperature in fahrenheit is %f", fahrenheit);
+ 
+// return 0;
+// }
+
+ #include<stdio.h>
+ #include<string.h>
+  int main(){
+     char day[] ="sunday";
+     if(strcmp(day, "sunday") == 0){ 
+        printf("Today is Funday");
+     } 
+  return 0; 
+ }
+
+ 
+ 
+
+ 
+
+ 
+
+
